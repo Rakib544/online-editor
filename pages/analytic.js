6c1efbc12b9eca9832d5681d33c0b2529aefb84b
SAGOR <seakingmawmaw2020@gmail.com>
2021-08-12 03:09:04 +0600

    update images

---
+++ pages/analytic.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Analytics from '../src/components/analytics/analytics';
+
+const Analytic = () => {
+    return (
+        <div className="container">
+            <Analytics/>
+        </div>
+    );
+};
+
+export default Analytic;
--- src/components/analytics/analyticsSideBar.js
+++ src/components/analytics/analyticsSideBar.js
@@ -1,16 +1,38 @@
 import React from "react";
 import styles from "./analyticsSideBar.module.css";
+import graph from "../../../src/utilites/images/graph.svg";
+import drop from "../../../src/utilites/images/drop.svg";
+import sound from "../../../src/utilites/images/sound.svg";
+import qr from "../../../src/utilites/images/qr.svg";
+import eye from "../../../src/utilites/images/eye.svg";
+import Image from "next/image";
 
 const AnalyticsSideBar = () => {
   return (
-    <div className={`${styles.analyticsSidebar}`}>
-      <p className={`${styles.overView} text-center py-2 text-white`}>
-        OverView
-      </p>
-      <p className="py-3">Analytics</p>
-      <p className="py-3">Template</p>
-      <p className="py-3">Campaign</p>
-      <p className="py-3">QR Code</p>
+    <div className="py-3" className="p-3">
+      <div className="py-3" className={`${styles.analyticsSidebar}`}>
+        
+        <div className="py-3" className={`${styles.overView} px-3 d-flex justify-content-between d-flex align-items-center text-white`}>
+        <Image src={eye} height="30" width="30" alt="graph" />
+          <span>OverView</span>
+        </div>
+        <div className="py-3">
+          <Image src={graph} height="30" width="30" alt="graph" />{" "}
+          <span>Analytics</span>
+        </div>
+        <div className="py-3">
+          <Image src={drop} height="30" width="30" alt="graph" />{" "}
+          <span>Template </span>
+        </div>
+        <div className="py-3">
+          <Image src={sound} height="30" width="30" alt="graph" />{" "}
+          <span>Campaign</span>
+        </div>
+        <div className="py-3">
+          <Image src={qr} height="30" width="30" alt="graph" />{" "}
+          <sapn>QR Code</sapn>
+        </div>
+      </div>
     </div>
   );
 };
